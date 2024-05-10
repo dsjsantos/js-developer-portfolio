0a5e645a34e035a9dc541aa64275707accef810e
Daniel Jegorschki Santos <dssantos@gmail.com>
2024-05-10 12:04:09 -0300

    Ajuste no portfolio para desafio DIO

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
-
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    //const url = "http://localhost:8080/data/profile.json";
+    const url = "https://raw.githubusercontent.com/dsjsantos/js-developer-portfolio/main/data/profile.json";
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
